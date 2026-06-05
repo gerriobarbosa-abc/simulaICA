c17d3f72c24d8a8752fe73bad28e5a5ebd0aeaf3
gerriobarbosa-abc <gerriobarbosa@abemcomum.org>
2026-06-05 12:01:56 -0300

    colocando dependencies

--- app.R
+++ app.R
@@ -15,7 +15,7 @@ library(scales)
 library(tibble)
 library(DT)
 library(purrr)
-requireNamespace("curl", quietly = TRUE)
+# requireNamespace("curl", quietly = TRUE)
 
 # -----------------------------------------------------------------------------
 # 2. Arquivos auxiliares
